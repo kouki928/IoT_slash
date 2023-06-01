2097729d8ac0c7320c7889b7830218a2f0b1321f
kouki928 <furukawavo@gmail.com>
2023-06-01 15:04:44 +0900

    add redirect

--- trashCan/urls.py
+++ trashCan/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="")]
+urlpatterns = [
+    path("", views.index, name="auth"),
+    path("login", views.login, name="")
+]
--- trashCan/views.py
+++ trashCan/views.py
@@ -1,7 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return render(request, "trashCan/index.html")
+    
+    if request.method == "GET":
+        user_state = request.user.is_authenticated
+        
+        return render(
+            request,
+            "trashCan/index.html",
+            {"user_state" : user_state}
+        )
+    
+    elif request.method == "POST":
+        
+        forms = request.POST["user_id"]
+        print(forms)
+        
+        return redirect("auth")
+
+    
+def login(request):
+    return HttpResponse()
