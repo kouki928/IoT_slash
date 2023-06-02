1923d6e31e4960c3388e0247d27f48a5836f247b
kouki928 <furukawavo@gmail.com>
2023-06-02 10:05:15 +0900

    signUp and logIn

--- trashCan/views.py
+++ trashCan/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -10,13 +10,14 @@ def index(request):
     if request.method == "GET":
         
         user_state = request.user.is_authenticated
-        print(user_state)
+        
         # index.html を描写 ---------------------------- #
         return render(
             request,
             "trashCan/index.html",
             {
-                "user_state" : user_state
+                "user_state" : user_state,
+                "error" : False
             }
         )
     
@@ -24,7 +25,11 @@ def index(request):
     elif request.method == "POST":
         
         forms = request.POST
-        user_state = forms["user_state"]
+        
+        if forms["inout"] == "logout":
+            logout(request)
+            return redirect("auth")
+        
         user_id = forms["user_id"]
         password = forms["password"]
 
@@ -35,7 +40,12 @@ def index(request):
             return redirect("auth")
         
         else:
-            return HttpResponse("Error")
+            return render(request,"trashCan/index.html",{
+                "user_state" : False,
+                "error" : True,
+                "user_id" : user_id,
+                "password" : password,
+            })
 
     
 def signUp(request):
@@ -46,6 +56,9 @@ def signUp(request):
         return render(
             request,
             "trashCan/signUp.html",
+            {
+                "error" : False,
+            }
         )
     
     elif request.method == "POST":
@@ -53,12 +66,21 @@ def signUp(request):
         user_id = forms["user_id"]
         password = forms["password"]
         
-        User.objects.create_user(username=user_id,password=password)
-        user = authenticate(request, username=user_id, password=password)
+        try:
+            existing_user = User.objects.get(username=user_id)
+            
+            return render(request, "trashCan/signUp.html",{
+                "user" : existing_user,
+                "error" : True
+            })
+        except:
         
-        if user is not None:
-            login(request, user)
-            return redirect("auth")
-        
-        else:
-            return HttpResponse("Error")
+            User.objects.create_user(username=user_id,password=password)
+            user = authenticate(request, username=user_id, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return redirect("auth")
+            
+            else:
+                return HttpResponse("Error")
